bbcb8c294bc178ee3ccdf88bf4e82014f20ec099
Jan-Erik Rediger <janerik@fnordig.de>
2018-09-30 00:16:42 +0200

    Generate code from slides.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,56 @@
+
 pub trait Jump {
     type Previous;
     type Next;
 }
 
+
+
 /// # lysbilder
-///
+/// 
 /// ## the slide system no one needed.
+/// 
 pub struct Slide1;
 impl Jump for Slide1 {
     type Previous = ();
     type Next = Slide2;
 }
 
+
 /// # Who am I?
+/// 
 pub struct Slide2;
 impl Jump for Slide2 {
     type Previous = Slide1;
+    type Next = Slide3;
+}
+
+
+/// # This slide deck can hold so much code.
+/// 
+/// ```rust
+/// extern crate panic_at_the_disco;
+/// 
+/// fn main() {
+///     panic_at_the_disco::init();
+/// 
+///     panic!("at the disco");
+/// }
+/// ```
+/// 
+pub struct Slide3;
+impl Jump for Slide3 {
+    type Previous = Slide2;
+    type Next = Slide4;
+}
+
+
+/// # The End
+/// 
+/// Please don't use it.
+pub struct Slide4;
+impl Jump for Slide4 {
+    type Previous = Slide3;
     type Next = ();
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
